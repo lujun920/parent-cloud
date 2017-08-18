51a625dd9cde28f46b47a46548e041f96667a8fa
lujun920 <lujun920@yeah.net>
2017-08-18 17:48:44 +0800

    添加spring boot admin 监控

---
+++ spring-boot-admin-ms/src/main/java/com/zt/ApplicationBootAdmin.java
@@ -0,0 +1,19 @@
+package com.zt;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class ApplicationBootAdmin {
+    public static void main( String[] args ) {
+        SpringApplication.run(ApplicationBootAdmin.class, args);
+    }
+}
